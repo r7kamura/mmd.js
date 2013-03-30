7bff20319412152b7ba4b653b2cdc18538f4dce9
Ryo Nakamura <r7kamura@gmail.com>
2013-03-30 19:52:52 +0900

    Change data path from absolute to relative form
    For gh-pages.

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,4 +1,4 @@
-Loader.load '/data/example.pmx', (arrayBuffer) ->
+Loader.load 'data/example.pmx', (arrayBuffer) ->
   parser     = new Parser(arrayBuffer)
   model      = parser.parse()
   controller = new Controller(model)
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function() {
 
-  Loader.load('/data/example.pmx', function(arrayBuffer) {
+  Loader.load('data/example.pmx', function(arrayBuffer) {
     var controller, model, parser;
     parser = new Parser(arrayBuffer);
     model = parser.parse();
